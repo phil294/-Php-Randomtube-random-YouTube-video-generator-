f0fac5fb15802e8ee01a5176d5b9d10c721c2a08
Philip Waritschlager <eochgls@web.de>
2017-02-25 21:49:58 +0100

    Create random.php

---
+++ random.php
@@ -0,0 +1,66 @@
+<?php
+
+$key = 'AIzaSyAkPe6-m_amqJ5VX6m6F2mJoDd5KYDsz8k';
+
+$cs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '-', '_'];
+$vids = [];
+$video = '';
+// try to find a video 10 times
+for ($k = 1; $k <= 10; $k++) {
+	$query = '';
+	// get 4 char random string
+	for ($i = 1; $i <= 4; $i++) {
+		$query .= $cs[rand(0, sizeof($cs) - 1)];
+	}
+
+	//$querystring = 'v%3D' . $q;
+	$url = 'https://www.googleapis.com/youtube/v3/search?part=id&maxResults=50&videoEmbeddable=true&type=video&q='
+		. $query . '&key=' . $key
+	//	. '&topicId=' . '/m/04rlf'  // music videos
+	//	. '&videoDuration=long'
+	// usw
+	;
+
+	$resp = file_get_contents($url);
+	$a = json_decode($resp, true);
+	if ($a["pageInfo"]["totalResults"] > 50) {
+		// continue?
+	}
+	if ($a["pageInfo"]["totalResults"] == 0) {
+		// nothing found
+		continue;
+	}
+
+	foreach ($a["items"] as $item) {
+		if (stripos($item["id"]["videoId"], $query) !== false) {
+			// q in vid id
+			$vids[] = $item["id"]["videoId"];
+		} else {
+			// q was somewhere else like description etc -> ignore video
+		}
+	}
+	if (sizeof($vids) > 0) {
+		break;
+	} else {
+		// nothing found
+
+		continue;
+	}
+}
+if (sizeof($vids) > 0) {
+	$videoId = $vids[rand(0, sizeof($vids) - 1)];
+	echo "<a href=\"http://youtube.com/watch?v=".$videoId."\">".$videoId."</a>";
+} else {
+	echo -1;
+}
+
+
+
+
+
+
+
+
+
+
+
